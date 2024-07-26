67989b8cd0fbb7e4ba9ec9a81087d61252fb14c3
Ateş <125479942+Emre4tes@users.noreply.github.com>
2024-07-26 13:52:18 +0300

    Update server.js

--- e-commerce-order-summary/Back-End/server.js
+++ e-commerce-order-summary/Back-End/server.js
@@ -8,7 +8,7 @@ const port = 3000;
 
 const uniqueIdentifier = '3b5c6d1e-8a6a-44c8-9baf-7a2b4c1e9c59';
 
-const authMiddleware = (req, res, next) => {
+const check = (req, res, next) => {
   const authHeader = req.headers['authorization'];
   console.log('Authorization Header:', authHeader);
   if (authHeader && authHeader === `Bearer ${uniqueIdentifier}`) {
@@ -22,7 +22,7 @@ const authMiddleware = (req, res, next) => {
 
 app.use(cors());
 app.use(bodyParser.json());
-app.use(authMiddleware);
+app.use(check);
 
 
 app.get('/order', (_req, res) => {
