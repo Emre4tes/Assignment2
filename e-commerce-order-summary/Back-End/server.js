6f74dbf3b823ab7498f2fd7c881d5be972c2cda0
Ateş <125479942+Emre4tes@users.noreply.github.com>
2024-08-07 13:40:20 +0300

    Code Optimization

--- e-commerce-order-summary/Back-End/server.js
+++ e-commerce-order-summary/Back-End/server.js
@@ -5,26 +5,21 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
-
 const uniqueIdentifier = '3b5c6d1e-8a6a-44c8-9baf-7a2b4c1e9c59';
 
-//Return http status 429 as 30% range
-function rateLimiter( _req,res, next)  {
-  if (Math.random() < 0.3) { // %30 oranında bir uygulama, her 10 isteğin 3'ünde HTTP 429 döndürecektir.
+// Return http status 429 as 30% range
+function rateLimiter(_req, res, next) {
+  if (Math.random() < 0.3) {
     return res.status(429).json({ error: 'Too Many Requests' });
   }
-  next(); // İstek başarılıysa devam et
-};
-
+  next();
+}
 
 const check = (req, res, next) => {
   const authHeader = req.headers['authorization'];
-  console.log('Authorization Header:', authHeader);
   if (authHeader && authHeader === `Bearer ${uniqueIdentifier}`) {
     next();
-    console.log('Valid Token');
   } else {
-    console.log('Unauthorized - Invalid Token');
     res.status(401).send('Unauthorized');
   }
 };
@@ -39,10 +34,8 @@ app.use(cors({
 
 app.use(rateLimiter);//İstemcilerin belirli bir süre içinde yapabilecekleri istek sayısını sınırlayarak, hizmetinizin kötüye kullanımını önler
 app.use(bodyParser.json());
-app.use(check);
 
-
-app.get('/order', (_req, res) => {
+app.get('/order', check, (_req, res) => {
   const order = [
     { id: 1, name: 'Cool Shirt', price: 25.0, qty: 3, weight: 0.5 },
     { id: 2, name: 'Cool Pants', price: 45.0, qty: 2, weight: 1 },
@@ -51,7 +44,7 @@ app.get('/order', (_req, res) => {
   res.json(order);
 });
 
-app.get('/shipping', (_req, res) => {
+app.get('/shipping', check, (_req, res) => {
   const shipping = {
     carrier: 'UPS',
     address: {
@@ -68,26 +61,14 @@ app.get('/shipping', (_req, res) => {
   res.json(shipping);
 });
 
-app.get('/tax', (_req, res) => {
+app.get('/tax', check, (_req, res) => {
   const tax = { amount: 0.07 };
   res.json(tax);
 });
 
-app.use((_req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-   // HTTP yanıtında CORS başlığı olarak 'Access-Control-Allow-Origin' ayarlanır
-  // '*' işareti, tüm kökenlerden gelen isteklerin kabul edileceğini belirtir
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-   // İstemcinin isteklerde kullanabileceği başlıkların listesini belirtir
-   // HTTP yanıtında CORS başlığı olarak 'Access-Control-Allow-Headers' ayarlanır
-  next();
-});
-
-
 app.use((_req, res) => {
   res.status(404).json({ message: 'Endpoint not found' });
 });
-
 
 app.listen(port, () => {
   console.log(`API running at http://localhost:${port}`);
